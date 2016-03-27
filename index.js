307ffcdf8a5edee46a1eb37457363a06e81e0a68
Nick Johnstone <ncwjohnstone@gmail.com>
2016-03-28 09:36:42 +1300

    Use keys for navigation, make the pink hotter

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ import {run} from '@cycle/core';
 import {makeDOMDriver} from '@cycle/dom';
 import {restart, restartable} from 'cycle-restart';
 import isolate from '@cycle/isolate';
-
+import {Observable} from 'rx';
+import keycode from 'keycode';
 var app = require('./src/app').default;
+
+function makeKeysDriver () {
+  const keydown$ = Observable.fromEvent(document, 'keydown');
+
+  function isKey (key) {
+    if (typeof key !== 'number') {
+      key = keycode(key);
+    }
+
+    return (event) => {
+      return event.keyCode === key;
+    };
+  }
+
+  return function keysDriver () {
+    return {
+      pressed: (key) => keydown$.filter(isKey(key))
+    };
+  };
+}
 
 const drivers = {
   DOM: restartable(makeDOMDriver('.app'), {pauseSinksWhileReplaying: false}),
+  Keys: restartable(makeKeysDriver())
 };
 
 const {sinks, sources} = run(app, drivers);
--- src/app.js
+++ src/app.js
@@ -25,13 +25,11 @@ function view (slide, slideIndex) {
 
 const cachedSlides = {};
 
-function slide(slideIndex, {DOM}) {
+function slide (slideIndex, {DOM}) {
   if (slideIndex in cachedSlides) {
-    console.log('cache hit for', slideIndex);
     return cachedSlides[slideIndex];
   }
 
-  console.log('cache miss for', slideIndex);
   const fetchedSlide = isolate(slides[slideIndex])({DOM});
 
   cachedSlides[slideIndex] = fetchedSlide;
@@ -39,20 +37,53 @@ function slide(slideIndex, {DOM}) {
   return fetchedSlide;
 }
 
-export default function App ({DOM}) {
-  const next$ = DOM
+function limit (result, boundingArray) {
+  const min = 0;
+  const max = boundingArray.length - 1;
+
+  if (result < min) {
+    return min;
+  }
+
+  if (result > max) {
+    return max;
+  }
+
+  return result;
+}
+
+export default function App ({DOM, Keys}) {
+  const clickNext$ = DOM
     .select('.next')
     .events('click')
-    .map(ev => +1);
 
-  const previous$ = DOM
+  const pressRight$ = Keys
+    .pressed('Right');
+
+  const pressSpace$ = Keys
+    .pressed('Space');
+
+  const next$ = Observable.merge(
+    clickNext$,
+    pressRight$,
+    pressSpace$
+  ).map(ev => +1);
+
+  const pressLeft$ = Keys
+    .pressed('Left');
+
+  const clickBack$ = DOM
     .select('.previous')
-    .events('click')
-    .map(ev => -1);
+    .events('click');
+
+  const previous$ = Observable.merge(
+    clickBack$,
+    pressLeft$
+  ).map(ev => -1);
 
   const slideIndex$ = next$.merge(previous$)
     .startWith(0)
-    .scan((total, change) => total + change);
+    .scan((total, change) => limit(total + change, slides));
 
   const slide$ = slideIndex$
     .map(slideIndex => slide(slideIndex, {DOM}));
